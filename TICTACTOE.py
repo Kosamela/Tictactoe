f5543e110fc2d2e01cdf2e4cfa72e4931899dda9
Kosamela <57198990+Kosamela@users.noreply.github.com>
2020-10-26 17:44:30 +0100

    Update TICTACTOE.py

--- TICTACTOE.py
+++ TICTACTOE.py
@@ -1,36 +1,67 @@
-a = input(str())
-print("---------")
-print("| " + a[0] + " " + a[1] + " " + a[2] + " |")
-print("| " + a[3] + " " + a[4] + " " + a[5] + " |")
-print("| " + a[6] + " " + a[7] + " " + a[8] + " |")
-print("---------")
 
-x_wins = False
-o_wins = False
-empty = "_" in a
-for i in range(0, 8, 3):
-    if a[i] == a[i + 1] == a[i + 2] == "X":
-        x_wins = True
-    elif a[i] == a[i + 1] == a[i + 2] == "O":
-        o_wins = True
-for i in range(3):
-    if a[i] == a[i + 3] == a[i + 6] == "X":
-        x_wins = True
-    elif a[i] == a[i + 3] == a[i + 6] == "O":
-        o_wins = True
-if a[0] == a[4] == a[8] == "X" or a[2] == a[4] == a[6] == "X":
-    x_wins = True
-if a[0] == a[4] == a[8] == "O" or a[2] == a[4] == a[6] == "O":
-    o_winx = True
-if a.count("X") - a.count("O") >= 2 or a.count("O") - a.count("X") >= 2:
-    print("Impossible")
-elif x_wins == True and o_wins == True:
-    print("Impossible")
-elif x_wins == False and o_wins == False and empty == False:
-    print("Draw")
-elif x_wins == True:
-    print("X wins")
-elif o_wins == True:
-    print("O wins")
-else:
-    print("Game not finished")
+b = [' ']
+
+map = [ 
+        [b[0], b[0], b[0]],
+        [b[0], b[0], b[0]],
+        [b[0], b[0], b[0]],
+    ]
+
+
+
+def play_map():
+    print("-" * 9)
+    print("| " + map[0][2] + " " + map[1][2] + " " + map[2][2] + " |")
+    print("| " + map[0][1] + " " + map[1][1] + " " + map[2][1] + " |")
+    print("| " + map[0][0] + " " + map[1][0] + " " + map[2][0] + " |")
+    print("-" * 9)
+
+play_map()
+
+def playing():
+    game_checker = True
+    number = 1
+
+    while game_checker == True:
+        ask = input("Enter the coordinates:")
+        move = ask.split(" ")
+        counter = number % 2
+
+        if move[0].isalpha() or move[1].isalpha():
+            print("You should enter numbers!")
+        elif int(move[0]) >= 4 or int(move[0]) <= 0 or int(move[1]) >= 4 or int(move[1]) <= 0:
+            print("Coordinates should be from 1 to 3!")
+        elif map[int(move[0]) - 1][int(move[1])- 1] == "X" or map[int(move[0]) - 1][int(move[1]) - 1] == "O":
+            print("This cell is occupied! Choose another one!")
+        else:
+            for x in range(1, 4):
+                for k in range(1, 4):
+                    if int(move[0]) == x and int(move[1]) == k and counter == 1:
+                        map[x - 1][k - 1] = "X"
+                        number += 1
+                        play_map()
+                    elif int(move[0]) == x and int(move[1]) == k and counter == 0:  
+                        map[x - 1][k - 1] = "O"
+                        number += 1
+                        play_map()                 
+                    if (map[x - 1][0] == map[x - 1][1] == map[x - 1][2] == 'X') or (map[2][k - 1] == map[1][k - 1] == map[0][k - 1] == "X"):
+                        print("X wins")
+                        game_checker = False
+                        break
+                    elif (map[x - 1][0] == map[x - 1][1] == map[x - 1][2] == 'O') or (map[2][k - 1] == map[1][k - 1] == map[0][k - 1] == "O"):
+                        print("O wins")
+                        game_checker = False
+                        break
+                    elif (map[0][0] == map[1][1] == map[2][2] == 'X') or (map[2][0] == map[1][1] == map[0][2] == "X"):
+                        print("X wins")
+                        game_checker = False
+                        break
+                    elif (map[0][0] == map[1][1] == map[2][2] == 'O') or (map[2][0] == map[1][1] == map[0][2] == "O"):
+                        print("O wins")
+                        game_checker = False
+                        break
+                    if number == 10 and game_checker == True:
+                        print("Draw")
+                        game_checker = False
+                        break
+playing()
